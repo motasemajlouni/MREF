17ad2bed1918ca74d1c7c0d35f9a41252cb5e143
ALMOTASEM BELLAH AL AJLOUNI <59858185+motasemajlouni@users.noreply.github.com>
2026-04-20 17:40:20 +0300

    Update __init__.py

--- MREF/__init__.py
+++ MREF/__init__.py
@@ -1,3 +1,2 @@
-from .mref_metric import MREF, AxisWeights, compare_one
-
-__all__ = ["MREF", "AxisWeights", "compare_one"]
+from .mref_metric import MREF, AxisWeights
+__all__ = ["MREF", "AxisWeights"]
